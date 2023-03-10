c2bea0ad4b6435aeb61dc6b3e72260ab1e6da1ed
spln <805133358@qq.com>
2023-03-10 13:25:43 +0800

    redis缓存答案查询结构功能回归

--- cache/result_cache.go
+++ cache/result_cache.go
@@ -20,30 +20,6 @@ func ExpectedResultCache(exerciseID int64, result []map[string]interface{}) {
 	}
 }
 
-// GetExpectedResult 从cache中读取exerciseID的结果返回. 成功bool类型返回true, 若不存在则bool类型返回值返回false，
-func GetExpectedResult(exerciseID int64) ([]map[string]interface{}, bool) {
-	cacheKey := fmt.Sprintf("ExpectedResultCache:%d", exerciseID)
-	exists, err := rdb.Exists(ctx, cacheKey).Result()
-	if err != nil {
-		log.Println(err)
-		return nil, false // 错误
-	}
-	if exists == 0 { // 该key不存在
-		return nil, false
-	}
-	resultBytes, err := rdb.Get(ctx, cacheKey).Bytes() // 从redis中获取数据
-	if err != nil {
-		log.Println(err)
-	}
-	// 将resultBytes还原成[]map[string]interface{}
-	var retrievedData []map[string]interface{}
-	err = json.Unmarshal(resultBytes, &retrievedData)
-	if err != nil {
-		log.Println(err)
-	}
-	return retrievedData, true
-}
-
 // GetExpectedResultBytes 从cache中读取exerciseID的结果返回. 成功bool类型返回true, 若不存在则bool类型返回值返回false，
 func GetExpectedResultBytes(exerciseID int64) ([]byte, bool) {
 	cacheKey := fmt.Sprintf("ExpectedResultCache:%d", exerciseID)
