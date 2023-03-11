9882dc860721ba0b4cf728e9b7a1a8ed99f29bc3
spln <805133358@qq.com>
2023-03-11 10:41:42 +0800

    定义获取当前题面信息接口

---
+++ controller/exercise/get_one_exercise_handle.go
@@ -0,0 +1,8 @@
+package exercise
+
+import "github.com/gin-gonic/gin"
+
+// GetOneExerciseHandle 获取当前题目的题目信息
+func GetOneExerciseHandle(context *gin.Context) {
+
+}
--- router/routers.go
+++ router/routers.go
@@ -66,6 +66,7 @@ func InitServer() *gin.Engine {
 		exerciseGroup.POST("/upload/table", middlewares.TeacherJWTMiddleware(), exercise.UploadTableHandle)         // 发布练习表单接口
 		exerciseGroup.POST("/submit/", middlewares.StudentJWTMiddleware(), exercise.SubmitHandle)                   // 处理提交习题接口
 		exerciseGroup.GET("/get/all/", exercise.GetAllExerciseHandle)                                               // 获取题库中所有可见的题目条目
+		exerciseGroup.GET("/get/one/", middlewares.StudentJWTMiddleware(), exercise.GetOneExerciseHandle)           // 获取当前题目的题面
 	}
 	submissionGroup := server.Group("/api/submission")
 	{
